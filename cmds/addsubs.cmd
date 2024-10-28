bfacec85e11736ee9102b196be33ded3bb6e6df0
unit-404 <conway24b@gmail.com>
2024-10-28 16:24:49 +0700

    update all packages

---
+++ cmds/addsubs.cmd
@@ -0,0 +1,19 @@
+git submodule add https://github.com/unite-2-ts/design.core ./modules/design.core
+git submodule add https://github.com/unite-2-ts/theme.core ./modules/theme.core
+git submodule add https://github.com/unite-2-ts/grid.core ./modules/grid.core
+git submodule add https://github.com/unite-2-ts/existence.core ./modules/existence.core
+git submodule add https://github.com/unite-2-ts/web.core ./modules/web.core
+
+git submodule add https://github.com/unite-2-ts/scrollbox.wcomp ./modules/scrollbox.wcomp
+git submodule add https://github.com/unite-2-ts/rows.wcomp ./modules/rows.wcomp
+git submodule add https://github.com/unite-2-ts/longtext.wcomp ./modules/longtext.wcomp
+
+git submodule add https://github.com/unite-2-ts/dom.ts ./modules/dom.ts
+git submodule add https://github.com/unite-2-ts/image.ts ./modules/image.ts
+git submodule add https://github.com/unite-2-ts/object.ts ./modules/object.ts
+git submodule add https://github.com/unite-2-ts/uniform.ts ./modules/uniform.ts
+git submodule add https://github.com/unite-2-ts/interact.ts ./modules/interact.ts
+git submodule add https://github.com/unite-2-ts/image.ts ./modules/image.ts
+git submodule add https://github.com/unite-2-ts/dom.ts ./modules/dom.ts
+
+pause
---
+++ install.cmd
@@ -0,0 +1,3 @@
+call pnpm install --shamefully-hoist -f -D
+call pnpm audit --audit-level moderate --fix
+pause
