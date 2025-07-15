b62c2f3bdc3464ce87c4d1397f7bd22e4ab4b161
u2re-dev <c24b@u2re.ru>
2025-07-15 19:32:40 +0700

    new factors

--- push.sh
+++ push.sh
@@ -6,16 +6,16 @@ directories=(./modules/*/* ./apps/* ./runtime)
 
 #
 for dir in "${directories[@]}"; do
-  if [ -d "$dir/.git" ]; then
-    session_name=$(basename "$dir")
-    screen -dmS "$session_name" bash -c "
-      cd $dir && \
-      git add . ; git add * ; \
-      git commit -m \"$message\" ; \
-      git push --all
-    "
-    echo "Started git operations in screen session: $session_name"
-  else
-    echo "Skipping $dir: not a git repository"
-  fi
+    if [ -d "$dir/.git" ]; then
+        session_name=$(basename "$dir")
+        screen -dmS "$session_name" bash -c "
+            cd $dir && \
+            git add . ; git add * ; \
+            git commit -m \"$message\" ; \
+            git push --all
+        "
+        echo "Started git operations in screen session: $session_name"
+    else
+        echo "Skipping $dir: not a git repository"
+    fi
 done
