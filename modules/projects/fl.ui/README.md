b02367900ba12e20208745261f260e353fdec5e5
u2re-dev <c24b@u2re.ru>
2025-08-31 11:02:20 +0700

    Broken Project

--- modules/projects/fl.ui/README.md
+++
@@ -1,3 +0,0 @@
-# 💐 FL.UI 💐
-
-🚧 full rehaul of our UI systems... full restart of UI project. 🚧
--- modules/projects/fl.ui/src/design/engine/Config.ts
+++
@@ -1,8 +0,0 @@
-export const electronAPI = "electronBridge";
-export const qualityMode = {
-    "fast": {
-        divisor: 4,
-        filter: "blur(4px)",
-        sampling: 128
-    }
-};
