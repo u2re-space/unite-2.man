8f3f31837c8569b1e752f628fe2a539bc73e1696
L2NE-dev <conway24b@gmail.com>
2026-06-21 12:23:42 +0700

    KVM in vain, useless, and everything is broken, in next time use previous commit as base...

--- modules/projects/cwsp-shared/src/index.ts
+++ modules/projects/cwsp-shared/src/index.ts
@@ -9,3 +9,4 @@ export * from "./cwsp-binary-airpad.ts";
 export * from "./cws-client-wire-defaults.ts";
 export * from "./clipboard-wire-constants.ts";
 export * from "./airpad-cwsp-client-parity.ts";
+export * from "./kvm-layout.ts";
